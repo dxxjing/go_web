d8abab34d8f42897617da75c97059cd876943e65
v_dxxjing <v_dxxjing@tencent.com>
2020-08-28 23:14:03 +0800

    update

--- day01/gorm/main.go
+++ day01/gorm/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
-	DB *sql.DB
+	db *sql.DB
 )
 
-func initdb() {
-	dsn := "root:123456@(127.0.0.1:3306)/db1"
-	db, err := sql.Open("mysql", dsn)
+func initDB() {
+	dsn := "root:root@(127.0.0.1:3306)/test"
+	var err error
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		fmt.Printf("sql open err %v\n", err)
 		return
@@ -23,8 +23,9 @@ func initdb() {
 		fmt.Printf("db ping err:%v\n", err)
 		return
 	}
+	fmt.Println("数据库连接成功!")
 }
 
 func main() {
-	initdb()
+	initDB()
 }
