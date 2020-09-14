9bb9c23f232276f2813d2a01bb47fc2ac7d39afa
dxxjing <43433077+dxxjing@users.noreply.github.com>
2020-09-14 17:01:23 +0800

    Update main.go

--- gin/async/main.go
+++ gin/async/main.go
@@ -10,19 +10,42 @@ import (
 func main(){
 
 	r := gin.Default()
-
+	//异步执行 无需waitgroup
 	r.GET("/async",func(c *gin.Context){
 		//注意：这里子goroutine必须使用gin.Context副本,否则会出问题
-		var wg sync.WaitGroup
-		tmpContext := c
+		//var wg sync.WaitGroup
+		tmpContext := c.Copy()
 		//异步执行
-		wg.Add(1)
+		fmt.Println("异步执行start....")
+		//wg.Add(1)
 		go func(){
-			defer wg.Done()
-			time.Sleep(time.Second*3)
-			fmt.Println("异步执行",tmpContext.Request.URL.Path)
+			//defer wg.Done()
+			time.Sleep(time.Second*5)
+			fmt.Println("异步执行end",tmpContext.Request.URL.Path)
 		}()
-		wg.Wait()
+		//wg.Wait()
+		fmt.Println("继续执行其他....")
+		//输出：
+		//异步执行start....
+		//继续执行其他....
+		//异步执行end
+	})
+	r.GET("/async",func(c *gin.Context){
+		//注意：这里子goroutine必须使用gin.Context副本,否则会出问题
+		//var wg sync.WaitGroup
+		tmpContext := c
+		//同步执行
+		fmt.Println("同步执行start....")
+		
+		time.Sleep(time.Second*5)
+		fmt.Println("同步执行end",tmpContext.Request.URL.Path)
+		
+		fmt.Println("继续执行其他....")
+		//输出：
+		//同步执行start....
+		//同步执行end
+		//继续执行其他....
+		
 	})
 
 	r.Run()
